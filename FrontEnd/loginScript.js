1ed932b4d99bcc84a8b92ced0fc73de54b8e4469
GenesisBlock2140 <combnathan02@gmail.com>
2024-03-08 15:36:24 +0100

    Adding login script

--- FrontEnd/loginScript.js
+++ FrontEnd/loginScript.js
@@ -3,14 +3,35 @@ document.addEventListener("DOMContentLoaded", function () {
   let emailInput = document.querySelector("#email");
   let passwordInput = document.querySelector("#password");
   submitBtn.addEventListener("click", () => {
-    console.log(emailInput.value, passwordInput.value);
+    tryLogin({ email: emailInput.value, password: passwordInput.value });
   });
 });
 
-const tryLogin = () => {
+async function tryLogin({ email, password }) {
+  const baseUrl = "http://localhost:5678";
+  const data = {
+    email,
+    password,
+  };
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
   try {
-    // tenter la connexion
+    const response = await fetch(baseUrl + "/api/users/login", options);
+    if (!response.ok) {
+      let errorMessage = document.querySelector(".login-error");
+      errorMessage.classList.add("login-error-visible");
+      console.log("Erreur Status", response.status);
+      throw new Error("Erreur serveur");
+    }
+    const responseData = await response.json();
+    localStorage.setItem("token", responseData.token);
+    window.location.href = "./index.html";
   } catch (error) {
-    console.log(error);
+    console.log("Erreur", error);
   }
-};
+}
